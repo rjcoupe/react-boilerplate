54efda9d2426af3297d222295605745c5f4a08c1
Richard Coupe <rcoupe@sorensonmedia.com>
2016-05-17 22:06:12 +0100

    Revert "God, so much"
    This reverts commit 8ce07b5a12737a92f362c7e3615c23dbeaa0a4e7.

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,37 +1,14 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import { IndexRoute, Route, Router, browserHistory } from 'react-router';
+var React = require('react');
+var ReactDOM = require('react-dom');
 
-// Components
-import Dashboard from './Dashboard';
-import Frontpage from './Frontpage';
+var Main = React.createClass({
+  render: function() {
+    return (
+      <div>
+        Hello world
+      </div>
+    )
+  }
+});
 
-import { readCookie, writeCookie } from '../utils/Cookie';
-
-window.api = 'http://localhost:8080/api';
-
-class Main extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div>
-                {this.props.children}
-            </div>
-        )
-    }
-}
-
-
-var indexComponent = readCookie('user') ? Dashboard : Frontpage;
-var indexComponent = Dashboard;
-
-render((
-    <Router>
-        <Route path="/" component={Main}>
-            <IndexRoute component={indexComponent} />
-        </Route>
-    </Router>
-), document.getElementById('app'));
+ReactDOM.render(<Main/>, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,7 @@
-var path = require('path');
-var webpack = require('webpack');
-
-var assetsPath = path.join(__dirname, 'public/assets');
-var distPath = './dist';
-
 module.exports = {
     entry: './app/components/Main.js',
     output: {
-        path: distPath,
-        filename: 'bundle.js'
+        filename: './public/bundle.js'
     },
     module: {
         loaders: [
@@ -23,13 +16,7 @@ module.exports = {
                test: /\.scss$/,
                exclude: /(node_modules|bower_components)/,
                loaders: ['style', 'css', 'sass']
-           }, {
-               test: /\.(jpe?g|png|gif|svg)$/i,
-               loaders: [
-                   'file?hash=sha512&digest=hex&name=[hash].[ext]',
-                   'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-               ]
-           }
+             }
         ]
     }
 };
