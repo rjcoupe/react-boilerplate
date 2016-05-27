955727ccae074c766b07aeb25f8675aa8ce4e56a
Richard Coupe <rcoupe@sorensonmedia.com>
2016-05-27 10:11:39 +0100

    Updated webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,52 @@ var path = require('path');
 var webpack = require('webpack');
 
 var assetsPath = path.join(__dirname, 'public/assets');
-var distPath = './dist';
+var distPath = '../server/public';
+
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
 
 module.exports = {
     entry: './app/components/Main.js',
     output: {
         path: distPath,
-        filename: 'bundle.js'
+        filename: 'bundle.[hash].js'
     },
     module: {
-        loaders: [
-             {
-                 test: /\.jsx?$/,
-                 exclude: /(node_modules|bower_components)/,
-                 loader: 'babel',
-                 query: {
-                     presets: ['react', 'es2015']
-                 }
-             }, {
-               test: /\.scss$/,
-               exclude: /(node_modules|bower_components)/,
-               loaders: ['style', 'css', 'sass']
-           }, {
-               test: /\.(jpe?g|png|gif|svg)$/i,
-               loaders: [
-                   'file?hash=sha512&digest=hex&name=[hash].[ext]',
-                   'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-               ]
-           }
-        ]
-    }
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel',
+            query: {
+                presets: ['react', 'es2015']
+            }
+        }, {
+            test: /\.scss$/,
+            exclude: /(node_modules|bower_components)/,
+            loaders: ['style', 'css', 'sass']
+        }, {
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            loaders: [
+                'file?hash=sha512&digest=hex&name=[hash].[ext]',
+                'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+            ]
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+        }, {
+            test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+        }, {
+            test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: 'file-loader'
+        }]
+    },
+    plugins: [
+        new ExtractTextPlugin('bundle.css'),
+        new HtmlWebpackPlugin({
+            title: 'Throwdown.online',
+            template: 'app/assets/index-template.ejs'
+        })
+    ]
 };
